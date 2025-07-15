a181f9f54a9db42eff82fba7bd6364c5a46917ec
Glory Day <alwaysgloryday@gmail.com>
2025-07-15 16:32:41 +0900

    Fix: Modified directories structure

--- RtanRPG/RtanRPG/Program.cs
+++ RtanRPG/RtanRPG/Program.cs
@@ -1,14 +1,10 @@
-﻿using RtanRPG.Utils;
-using System.Text.Json;
-using System.IO;
-
-namespace RtanRPG
+﻿namespace RtanRPG
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-
+            Console.WriteLine("Hello, World!");
         }
     }
 }
