336904f198a8bd3fa91297db37502974618baeff
Glory Day <96814373+Glory-Day@users.noreply.github.com>
2025-07-15 16:10:02 +0900

    Update Program.cs

--- RtanRPG/RtanRPG/Program.cs
+++ RtanRPG/RtanRPG/Program.cs
@@ -8,20 +8,7 @@ namespace RtanRPG
     {
         static void Main(string[] args)
         {
-            GameConfig config = new GameConfig();
-            config.PlayerName = "나";
-            config.Level = 1;
-            config.Experience = 1;
-            JsonHandler<GameConfig> jsonHandler = new JsonHandler<GameConfig>();
-            string folderPath = Path.Combine(Environment.CurrentDirectory, "SaveData");
-            Directory.CreateDirectory(folderPath); 
-            string filePath = Path.Combine(folderPath, "config.json");
-
-            jsonHandler.Save(config, filePath);
-            config = jsonHandler.Load(filePath);
-            Console.WriteLine($"{config.PlayerName}");
-            Console.WriteLine($"{folderPath}");
-
+            
         }
     }
 }
