7e78a0686d32282b0d0bcffda13829e1ca386d67
Heoyoon <parechae@gmail.com>
2025-07-17 23:12:46 +0900

    [feat] : 캐릭터 부모 오브젝트 추가

---
+++ RtanRPG/FSM/Charactors/Charactor.cs
@@ -0,0 +1,48 @@
+﻿using RtanRPG.Object;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RtanRPG.FSM.Charactors
+{
+    class Charactor : MonoBehavior
+    {
+        protected Stat stat;
+        protected IStateMachine stateMachine;
+        public char[] keys;
+
+        public Charactor(Stat stat, char[] keys)
+        {
+            this.keys = keys;
+            this.stat = stat;
+            stateMachine = new MonsterStateMachine(stat);
+
+        }
+        public override void Awake()
+        {
+            base.Awake();
+        }
+        public override void Destroy()
+        {
+            
+        }
+        public override void Update()
+        {
+            
+        }
+        public override void Start()
+        {
+            
+        }
+        public override void Enable()
+        {
+            
+        }
+        public override void Disable()
+        {
+            
+        }
+    }
+}
