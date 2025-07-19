ee4a931c20b00c7ca44e9c147a3526b8454b934d
원정우 <rjstn155@naver.com>
2025-07-20 00:11:07 +0900

    Feature/ MaxHp > Hp

--- RtanRPG/Data/CharacterData.cs
+++ RtanRPG/Data/CharacterData.cs
@@ -9,26 +9,25 @@ namespace RtanRPG.Data
 {
     public class CharacterData
     {
-        private int maxHp;
-
-        public int MaxHp
+        private int healthPoint;
+        public int HealthPoint
         {
-            get => maxHp;
-            set => maxHp = Math.Clamp(value, 0, 100);  // 0~100 사이로 제한
+            get => healthPoint;
+            set => healthPoint = Math.Clamp(value, 0, MaxHp);
         }
 
         public string Name { get; set; }
         public int Level { get; set; }
         public string Class { get; set; }
-        public int HealthPoint { get; set; }
+        public int MaxHp {  get; set; }
         public int AttackPoint { get; set; }
         public int DefensePoint { get; set; }
         public int Gold { get; set; }
 
         public CharacterData()
         {
-            MaxHp = 100;          // 기본 최대체력
-            HealthPoint = MaxHp;  // 현재 체력은 최대체력으로 초기화
+            MaxHp = 100;
+            HealthPoint = MaxHp;
         }
     }
 }
