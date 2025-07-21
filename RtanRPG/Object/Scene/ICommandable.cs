e672181bf4e93da4ff3fced0a21055edcc05854c
Glory Day <alwaysgloryday@gmail.com>
2025-07-21 09:12:56 +0900

    Fix: Reset project

--- RtanRPG/Object/Scene/ICommandable.cs
+++
@@ -1,9 +0,0 @@
-namespace RtanRPG.Object.Scene
-{
-    public interface ICommandable
-    {
-        void Execute(ConsoleKey key);
-
-        Dictionary<ConsoleKey, Action?> Commands { get; }
-    }
-}
